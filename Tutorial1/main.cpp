c91508a1ad126de6895ec88d591f55db095122e4
Tam Phan <124404261+tomoeri@users.noreply.github.com>
2023-09-05 23:09:36 -0700

    Finish T1

--- Tutorial1/main.cpp
+++ Tutorial1/main.cpp
@@ -1,11 +1,38 @@
-#include "mainwindow.h"
-
 #include <QApplication>
+#include <QGraphicsScene>
+#include <QGraphicsRectItem>
+#include <QGraphicsView>
+
+/* Prereqs
+ * -basic knowledge of C++ (pointers and memory management)
+ * -basic knowledge of Qt
+ * -- Tutorial:
+ * -QGraphicScene
+ * -QGraphicsItem (QGraphicsRectItem)
+ * -QGraphicsView
+ */
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+
+    // Create a scene
+    QGraphicsScene* scene = new QGraphicsScene();
+
+    // Create an item to put inot the scene
+    QGraphicsRectItem* rect = new QGraphicsRectItem();
+    rect->setRect(0, 0, 100, 100);
+
+    // Add the item to the scene
+    scene->addItem(rect);
+
+    // Add a view
+    QGraphicsView* view = new QGraphicsView(scene);
+    // view->setScene(scene);
+
+    view->show();
+
+
+
     return a.exec();
 }
